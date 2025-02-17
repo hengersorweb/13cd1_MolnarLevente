5922244401d87474dcdebb8f6da7afe46af2bf0a
mollev545 <mollev545@hengersor.hu>
2025-02-17 11:29:38 +0100

    szükséges modulok import
    bootstrap,route,tachyons

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = (()=>{
+
+    return(
+    <div>
+        <h1>Menu</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione earum cumque et enim qui at dignissimos! Temporibus amet corporis, quos tenetur accusantium eaque alias voluptate perferendis! Expedita consequatur optio ipsa rem soluta, fuga sunt laudantium reiciendis in officia architecto molestiae corrupti quo necessitatibus ut quasi, sint aliquid porro nostrum praesentium! Facilis rem nisi necessitatibus nesciunt iusto, est tenetur architecto, autem iste obcaecati similique nostrum nam optio accusantium illum laboriosam maiores voluptatum error pariatur commodi laborum asperiores qui. Ipsum facilis ut repellat possimus accusantium eius deleniti, sit molestiae eaque ab accusamus. Eos eligendi quisquam a quos aspernatur accusamus, officiis dolorem architecto!</p>
+    </div>
+)
+
+
+}
+)
+export default Navbar
