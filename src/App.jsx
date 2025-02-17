829ac27ea420fc7298e1910bb263bc06be2d1d24
mollev545 <mollev545@hengersor.hu>
2025-02-17 11:43:20 +0100

    router beállítása

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Home from "./oldalak/Home"
 import Ermesek from "./oldalak/Ermesek"
 import Mermesek from "./oldalak/Mermesek"
 import Navbar from "./Navbar"
-import { Routes,Route } from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css" 
+import { Routes, Route } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import "tachyons"
 
@@ -12,7 +12,13 @@ function App() {
   return (
     <>
       <div>
-      <h1>Covid</h1>
+        <h1>Covid</h1>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/ermesek" element={<Ermesek />}></Route>
+          <Route path="/mermesek" element={<Mermesek />}></Route>
+
+        </Routes>
       </div>
     </>
   )
